7f5f6317e6c074b5e62fb5e256cecfec3f4a7adb
osada hanaka <hanaka.ve732@icloud.com>
2026-08-27 23:03:20 +0900

    feat: 書籍一覧・詳細・登録画面の表示を実装

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+use App\Models\Genre;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::with('genres')
+            ->withAvg('reviews','rating')
+            ->paginate(10);
+
+        return view('books.index', compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $genres = Genre::all();
+
+        return view('books.create', compact('genres'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        $book ->load([
+            'genres',
+            'reviews.user',
+            'reviews.likedByUsers'
+            ]);
+
+        return view('books.show', compact('book'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('books.index');
-});
+Route::get('/', [BookController::class, 'index' ])
+->name('books.index');
 
-Route::get('/books/create', function () {
-    return '仮の画面';
-})->name('books.create');
+Route::get('/books/create', [BookController::class, 'create'])
+    ->middleware('auth')
+    ->name('books.create');
+
+Route::get('/books/{book}', [BookController::class, 'show'])
+    ->name('books.show');
+
+Route::post('/books',
+[BookController::class, 'store'])
+    ->middleware('auth')
+    ->name('books.store');
+
+
+
+// TODO: 仮ルート（お気に入り機能実装時にControllerへ変更）
+Route::post('/books/{book}/favorites', function () {
+    //
+})->middleware('auth')->name('favorites.toggle');
+
+// TODO: 仮ルート（レビュー投稿機能実装時にControllerへ変更）
+Route::post('/books/{book}/reviews', function () {
+    //
+})->middleware('auth')->name('reviews.store');
+
+// TODO: 仮ルート（レビューいいね機能実装時にControllerへ変更）
+Route::post('/reviews/{review}/like', function () {
+    //
+})->middleware('auth')->name('reviews.like');
+
+// TODO: 仮ルート（機能実装時にControllerへ変更）
+Route::get('/ranking', function () {
+    return 'ランキング';
+})->name('ranking.index');
+
+Route::get('/favorites', function () {
+    return 'お気に入り';
+})->name('favorites.index');
+
+Route::get('/genres', function () {
+    return 'ジャンル管理';
+})->name('genres.index');
