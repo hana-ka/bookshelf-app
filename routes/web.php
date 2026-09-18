a87a73f13d7053c26235ec2c27f0c05a4bea7470
osada hanaka <hanaka.ve732@icloud.com>
2026-09-18 15:12:02 +0900

    feat: 読書計画機能を追加

--- routes/web.php
+++ routes/web.php
@@ -21,109 +21,166 @@ use App\Http\Controllers\ReadingPlanController;
 |
 */
 
-Route::get('/', [BookController::class, 'index' ])
-->name('books.index');
+/*
+|--------------------------------------------------------------------------
+| Public Pages
+|--------------------------------------------------------------------------
+*/
 
-Route::get('/books/create', [BookController::class, 'create'])
-    ->middleware('auth')
-    ->name('books.create');
+// Book List
+Route::get('/', [BookController::class, 'index'])
+    ->name('books.index');
 
+// Book Detail
 Route::get('/books/{book}', [BookController::class, 'show'])
     ->name('books.show');
 
-Route::post('/books',
-[BookController::class, 'store'])
-    ->middleware('auth')
-    ->name('books.store');
-
-Route::post('/books/{book}/favorites',[FavoriteController::class, 'toggle'])
-    ->middleware('auth')
-    ->name('favorites.toggle');
-
-Route::post('/books/{book}/reviews', [ReviewController::class, 'store'])
-    ->middleware('auth')
-    ->name('reviews.store');
-
-Route::post('/reviews/{review}/like', [ReviewLikeController::class, 'toggle'])
-    ->middleware('auth')
-    ->name('reviews.like');
-
-Route::get('/reviews/{review}/edit', [ReviewController::class, 'edit'])
-    ->middleware('auth')
-    ->name('reviews.edit');
-
-Route::delete('/reviews/{review}', [ReviewController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('reviews.destroy');
-
-Route::get('/books/{book}/edit', [BookController::class, 'edit'])
-    ->middleware('auth')
-    ->name('books.edit');
-
-Route::delete('/books/{book}', [BookController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('books.destroy');
-
-Route::put('/reviews/{review}', [ReviewController::class, 'update'])
-    ->middleware('auth')
-    ->name('reviews.update');
-
-Route::put('/books/{book}', [BookController::class, 'update'])
-    ->middleware('auth')
-    ->name('books.update');
-
+// Ranking
 Route::get('/ranking', [RankingController::class, 'index'])
     ->name('ranking.index');
 
-Route::get('/favorites', [FavoriteController::class, 'index'])
-    ->middleware('auth')
-    ->name('favorites.index');
-
-Route::get('/genres', [GenreController::class, 'index'])
-    ->middleware('auth')
-    ->name('genres.index');
-
-Route::get('/genres/create', [GenreController::class, 'create'])
-    ->middleware('auth')
-    ->name('genres.create');
-
-Route::post('/genres', [GenreController::class, 'store'])
-    ->middleware('auth')
-    ->name('genres.store');
-
-Route::get('/genres/{genre}', [GenreController::class, 'show'])
-    ->middleware('auth')
-    ->name('genres.show');
-
-Route::get('/genres/{genre}/edit', [GenreController::class, 'edit'])
-    ->middleware('auth')
-    ->name('genres.edit');
-
-Route::put('/genres/{genre}', [GenreController::class, 'update'])
-    ->middleware('auth')
-    ->name('genres.update');
-
-Route::delete('/genres/{genre}', [GenreController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('genres.destroy');
-
+// ISBN Search API
 Route::get('/books/isbn/{isbn}', [BookController::class, 'isbn']);
 
-Route::get('/reports', [ReportController::class, 'index'])
-    ->middleware('auth')
-    ->name('reports.index');
 
-Route::get('/reading-plans', [ReadingPlanController::class, 'index'])
-    ->middleware('auth')
-    ->name('reading-plans.index');
+/*
+|--------------------------------------------------------------------------
+| Authenticated Pages
+|--------------------------------------------------------------------------
+*/
 
-Route::get('/reading-plans/create', [ReadingPlanController::class, 'index'])
-    ->middleware('auth')
-    ->name('reading-plans.create');
+Route::middleware('auth')->group(function () {
+
+    /*
+    |--------------------------------------------------------------------------
+    | Books
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/books/create', [BookController::class, 'create'])
+        ->name('books.create');
+
+    Route::post('/books', [BookController::class, 'store'])
+        ->name('books.store');
+
+    Route::get('/books/{book}/edit', [BookController::class, 'edit'])
+        ->name('books.edit');
+
+    Route::put('/books/{book}', [BookController::class, 'update'])
+        ->name('books.update');
+
+    Route::delete('/books/{book}', [BookController::class, 'destroy'])
+        ->name('books.destroy');
 
 
-//仮
-Route::get('/notifications', [BookController::class, 'index'])
-    ->middleware('auth')
-    ->name('notifications.index');
+    /*
+    |--------------------------------------------------------------------------
+    | Favorites
+    |--------------------------------------------------------------------------
+    */
 
+    Route::get('/favorites', [FavoriteController::class, 'index'])
+        ->name('favorites.index');
+
+    Route::post('/books/{book}/favorites', [FavoriteController::class, 'toggle'])
+        ->name('favorites.toggle');
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Reviews
+    |--------------------------------------------------------------------------
+    */
+
+    Route::post('/books/{book}/reviews', [ReviewController::class, 'store'])
+        ->name('reviews.store');
+
+    Route::get('/reviews/{review}/edit', [ReviewController::class, 'edit'])
+        ->name('reviews.edit');
+
+    Route::put('/reviews/{review}', [ReviewController::class, 'update'])
+        ->name('reviews.update');
+
+    Route::delete('/reviews/{review}', [ReviewController::class, 'destroy'])
+        ->name('reviews.destroy');
+
+    Route::post('/reviews/{review}/like', [ReviewLikeController::class, 'toggle'])
+        ->name('reviews.like');
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Genres
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/genres', [GenreController::class, 'index'])
+        ->name('genres.index');
+
+    Route::get('/genres/create', [GenreController::class, 'create'])
+        ->name('genres.create');
+
+    Route::post('/genres', [GenreController::class, 'store'])
+        ->name('genres.store');
+
+    Route::get('/genres/{genre}', [GenreController::class, 'show'])
+        ->name('genres.show');
+
+    Route::get('/genres/{genre}/edit', [GenreController::class, 'edit'])
+        ->name('genres.edit');
+
+    Route::put('/genres/{genre}', [GenreController::class, 'update'])
+        ->name('genres.update');
+
+    Route::delete('/genres/{genre}', [GenreController::class, 'destroy'])
+        ->name('genres.destroy');
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | My Reading Report
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/reports', [ReportController::class, 'index'])
+        ->name('reports.index');
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Reading Plans
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/reading-plans', [ReadingPlanController::class, 'index'])
+        ->name('reading-plans.index');
+
+    Route::get('/reading-plans/create', [ReadingPlanController::class, 'create'])
+        ->name('reading-plans.create');
+
+    Route::post('/reading-plans/{plan}/complete', [ReadingPlanController::class, 'complete'])
+        ->name('reading-plans.complete');
+
+    Route::get('/reading-plans/{plan}/edit', [ReadingPlanController::class, 'edit'])
+        ->name('reading-plans.edit');
+
+    Route::delete('/reading-plans/{plan}', [ReadingPlanController::class, 'destroy'])
+        ->name('reading-plans.destroy');
+
+    Route::post('/reading-plans', [ReadingPlanController::class, 'store'])
+        ->name('reading-plans.store');
+
+    Route::put('/reading-plans/{plan}', [ReadingPlanController::class, 'update'])
+        ->name('reading-plans.update');
+
+    /*
+    |--------------------------------------------------------------------------
+    | Notifications
+    |--------------------------------------------------------------------------
+    */
+
+    // Temporary
+    Route::get('/notifications', [BookController::class, 'index'])
+        ->name('notifications.index');
+
+});
