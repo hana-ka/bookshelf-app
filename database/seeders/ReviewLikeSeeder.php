5182f22deb849e93aa9a70605fe7dc812cc6954e
osada hanaka <hanaka.ve732@icloud.com>
2026-09-18 12:17:35 +0900

    feat: 応用機能向けのシードデータを追加

--- database/seeders/ReviewLikeSeeder.php
+++ database/seeders/ReviewLikeSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Review;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class ReviewLikeSeeder extends Seeder
@@ -12,46 +13,20 @@ class ReviewLikeSeeder extends Seeder
      */
     public function run(): void
     {
-        $reviewLikes = [
-            1 => [2, 3],
-            2 => [1],
-            3 => [],
-            4 => [2, 5],
-            5 => [1],
-            6 => [1, 4],
-            7 => [2],
-            8 => [3, 4],
-            9 => [5],
-            10 => [3],
-            11 => [],
-            12 => [1, 2],
-            13 => [5],
-            14 => [2, 4],
-            15 => [],
-            16 => [4],
-            17 => [1, 5],
-            18 => [3],
-            19 => [2],
-            20 => [5],
-            21 => [2, 4],
-            22 => [1],
-            23 => [3, 5],
-            24 => [],
-            25 => [4],
-            26 => [2],
-            27 => [1, 5],
-            28 => [4],
-            29 => [],
-            30 => [1, 3],
-            31 => [2],
-            32 => [4],
-        ];
-
+        $users = User::all();
         $reviews = Review::all();
 
         foreach ($reviews as $review) {
+            $likeCount = $likeCount = rand(0, 5);
+
+            if ($likeCount === 0) {
+                continue;
+            }
+
+            $likedUsers = $users->random($likeCount);
+
             $review->likedByUsers()->syncWithoutDetaching(
-                $reviewLikes[$review->id]
+                $likedUsers->pluck('id')->toArray()
             );
         }
     }
